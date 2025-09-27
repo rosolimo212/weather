379e48bf82dab91f13f50d051a1e26f1ad4d6c80
Roman Tsaregorodtsev <tsaregorodcev.r@litres.ru>
2025-09-27 19:51:05 +0300

    make yandex gpt

--- gpt.py
+++ gpt.py
@@ -1,3 +1,4 @@
+from locale import strcoll
 import requests
 
 import os
@@ -10,35 +11,42 @@ sys.path.append(current_dir)
 import weth_api as wa
 import data_load as dl
 
-settings = dl.read_yaml_config('config.yaml', section='gpt')
-api_key = settings['api_key']
+# settings = dl.read_yaml_config('config.yaml', section='gpt')
+# api_key = settings['api_key']
+# catalog_id = settings['catalog_id']
 
-def send_message(api_key, message, model='gpt-4o-mini'):
-    """
-    Send message to opeanai model and get the text of its response
-    """
-    url = 'https://api.openai.com/v1/chat/completions'
+def send_message(
+    text: str,
+    API_KEY: str,
+    CATALOG_ID: str,
+    role: str = "user",
+    temperature: float = 0.4,
+    max_tokens: int = 8000,
+):
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 
     headers = {
-        'Authorization': f'Bearer {api_key}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Api-Key {API_KEY}",
+        "x-folder-id": CATALOG_ID,
+        "Content-Type": "application/json"
     }
 
     data = {
-        'model': model,
-        'messages': [
-            {'role': 'user', 'content': message}
+        "modelUri": f"gpt://{CATALOG_ID}/yandexgpt-lite",
+        "messages": [
+            {"role": role, "text": text}
         ],
+        "temperature": temperature,
+        "maxTokens": max_tokens
     }
 
-    response = requests.post(url, headers=headers, json=data)    
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        answer = response.json()['result']['alternatives'][0]['message']['text']
 
-    res = ''
+    except Exception as e:
+        print("🚫 Status code:", response.status_code)
+        print(str(e))
+        answer = "YandexGPT не отвечает"
 
-    if response.status_code == 200:
-        res = response.json()['choices'][0]['message']['content']
-    else:
-        print('Error:', response.status_code)
-        print(response.text)   
-
-    return res
+    return answer
